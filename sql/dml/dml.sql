691ef052345934939ceae3713dbd707927d180e0
Isaac <isaacgarza432@gmail.com>
2019-05-04 15:25:22 -0500

    GH-3: Added DMLs to the sql folder
    - Added insert statements for the following tables:
       - funding_schedule_type
       - category
       - subcategory
    - The insert statements for these tables are the proposed data that these tables will contain going forward

---
+++ sql/dml/dml.sql
@@ -0,0 +1,29 @@
+insert into funding_schedule_type(id, type)
+values (uuid_to_bin('8bdd707e-6d63-11e9-8d1c-ca6496c4120e'), 'DAILY'),
+       (uuid_to_bin('8bdd729a-6d63-11e9-8d1c-ca6496c4120e'), 'WEEKLY'),
+       (uuid_to_bin('8bdd5922-6d63-11e9-8d1c-ca6496c4120e'), 'BI_WEEKLY'),
+       (uuid_to_bin('8bdd71a0-6d63-11e9-8d1c-ca6496c4120e'), 'MONTHLY'),
+       (uuid_to_bin('8bdd6ed0-6d63-11e9-8d1c-ca6496c4120e'), 'CUSTOM');
+
+insert into category(id, name)
+values (uuid_to_bin('ec849198-6d6b-11e9-8d1c-ca6496c4120e'), 'Auto & Transport'),
+       (uuid_to_bin('ec849616-6d6b-11e9-8d1c-ca6496c4120e'), 'Bills & Utilities'),
+       (uuid_to_bin('ec84988c-6d6b-11e9-8d1c-ca6496c4120e'), 'Education'),
+       (uuid_to_bin('ec8499e0-6d6b-11e9-8d1c-ca6496c4120e'), 'Entertainment'),
+       (uuid_to_bin('ec849aee-6d6b-11e9-8d1c-ca6496c4120e'), 'Fees & Charges'),
+       (uuid_to_bin('ec849cc4-6d6b-11e9-8d1c-ca6496c4120e'), 'Food & Drink'),
+       (uuid_to_bin('ec849ddc-6d6b-11e9-8d1c-ca6496c4120e'), 'Gifts & Donations'),
+       (uuid_to_bin('ec849ed6-6d6b-11e9-8d1c-ca6496c4120e'), 'Health & Fitness'),
+       (uuid_to_bin('ec849fe4-6d6b-11e9-8d1c-ca6496c4120e'), 'Home'),
+       (uuid_to_bin('ec84a0f2-6d6b-11e9-8d1c-ca6496c4120e'), 'Income'),
+       (uuid_to_bin('ec84a1ec-6d6b-11e9-8d1c-ca6496c4120e'), 'Investments/Savings'),
+       (uuid_to_bin('ec84a368-6d6b-11e9-8d1c-ca6496c4120e'), 'Kids'),
+       (uuid_to_bin('ec84a462-6d6b-11e9-8d1c-ca6496c4120e'), 'Misc Expenses'),
+       (uuid_to_bin('ec84a5ac-6d6b-11e9-8d1c-ca6496c4120e'), 'Personal Care'),
+       (uuid_to_bin('ec84a746-6d6b-11e9-8d1c-ca6496c4120e'), 'Pets'),
+       (uuid_to_bin('ec84a8ea-6d6b-11e9-8d1c-ca6496c4120e'), 'Shopping'),
+       (uuid_to_bin('ec84aaa2-6d6b-11e9-8d1c-ca6496c4120e'), 'Taxes'),
+       (uuid_to_bin('ec84ac3c-6d6b-11e9-8d1c-ca6496c4120e'), 'Transfer'),
+       (uuid_to_bin('ec84ad9a-6d6b-11e9-8d1c-ca6496c4120e'), 'Travel'),
+       (uuid_to_bin('ec84ae94-6d6b-11e9-8d1c-ca6496c4120e'), 'Uncategorized');
+
---
+++ sql/dml/subcategory_dml.sql
@@ -0,0 +1,137 @@
+# Auto & Transport
+insert into subcategory(id, category_id, name)
+values (uuid_to_bin('80e597b2-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849198-6d6b-11e9-8d1c-ca6496c4120e'), 'Auto Insurance'),
+       (uuid_to_bin('80e59b36-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849198-6d6b-11e9-8d1c-ca6496c4120e'), 'Auto Payment'),
+       (uuid_to_bin('80e59cb2-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849198-6d6b-11e9-8d1c-ca6496c4120e'), 'Gas & Fuel'),
+       (uuid_to_bin('80e59e88-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849198-6d6b-11e9-8d1c-ca6496c4120e'), 'Parking'),
+       (uuid_to_bin('80e5a054-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849198-6d6b-11e9-8d1c-ca6496c4120e'), 'Public Transportation'),
+       (uuid_to_bin('80e5a11c-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849198-6d6b-11e9-8d1c-ca6496c4120e'), 'Rideshare'),
+       (uuid_to_bin('80e5a1bc-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849198-6d6b-11e9-8d1c-ca6496c4120e'), 'Service & Parts');
+
+# Bills & Utilities
+insert into subcategory(id, category_id, name)
+values (uuid_to_bin('80f6e06c-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849616-6d6b-11e9-8d1c-ca6496c4120e'), 'Cable'),
+       (uuid_to_bin('80f6e4ea-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849616-6d6b-11e9-8d1c-ca6496c4120e'), 'Electricity'),
+       (uuid_to_bin('80f6e648-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849616-6d6b-11e9-8d1c-ca6496c4120e'), 'Internet'),
+       (uuid_to_bin('80f6e74c-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849616-6d6b-11e9-8d1c-ca6496c4120e'), 'Phone'),
+       (uuid_to_bin('80f6e850-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849616-6d6b-11e9-8d1c-ca6496c4120e'), 'Streaming Services'),
+       (uuid_to_bin('80f6e95e-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849616-6d6b-11e9-8d1c-ca6496c4120e'), 'Utilities');
+
+# Education
+insert into subcategory(id, category_id, name)
+values (uuid_to_bin('80fddd36-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84988c-6d6b-11e9-8d1c-ca6496c4120e'), 'Books & Supplies'),
+       (uuid_to_bin('80fde09c-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84988c-6d6b-11e9-8d1c-ca6496c4120e'), 'Student Housing'),
+       (uuid_to_bin('80fde18c-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84988c-6d6b-11e9-8d1c-ca6496c4120e'), 'Student Loan'),
+       (uuid_to_bin('80fde240-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84988c-6d6b-11e9-8d1c-ca6496c4120e'), 'Tuition');
+
+# Entertainment
+insert into subcategory(id, category_id, name)
+values (uuid_to_bin('810a69b6-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec8499e0-6d6b-11e9-8d1c-ca6496c4120e'), 'Amusement'),
+       (uuid_to_bin('810a6d94-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec8499e0-6d6b-11e9-8d1c-ca6496c4120e'), 'Misc Entertainment'),
+       (uuid_to_bin('810a6ea2-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec8499e0-6d6b-11e9-8d1c-ca6496c4120e'), 'Movies'),
+       (uuid_to_bin('810a7050-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec8499e0-6d6b-11e9-8d1c-ca6496c4120e'), 'Music'),
+       (uuid_to_bin('810a710e-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec8499e0-6d6b-11e9-8d1c-ca6496c4120e'), 'News & Magazines');
+
+# Fees & Charges
+insert into subcategory(id, category_id, name)
+values (uuid_to_bin('8114c852-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849aee-6d6b-11e9-8d1c-ca6496c4120e'), 'ATM Fee'),
+       (uuid_to_bin('8114cd8e-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849aee-6d6b-11e9-8d1c-ca6496c4120e'), 'Bank Fee'),
+       (uuid_to_bin('8114cf3c-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849aee-6d6b-11e9-8d1c-ca6496c4120e'), 'Late Fee'),
+       (uuid_to_bin('8114d07c-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849aee-6d6b-11e9-8d1c-ca6496c4120e'), 'Service Fee'),
+       (uuid_to_bin('8114d19e-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849aee-6d6b-11e9-8d1c-ca6496c4120e'), 'Trade Commissions');
+
+# Food & Drink
+insert into subcategory(id, category_id, name)
+values (uuid_to_bin('81217eb2-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849cc4-6d6b-11e9-8d1c-ca6496c4120e'), 'Bars & Clubs'),
+       (uuid_to_bin('8121822c-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849cc4-6d6b-11e9-8d1c-ca6496c4120e'), 'Coffee Beans'),
+       (uuid_to_bin('8121831c-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849cc4-6d6b-11e9-8d1c-ca6496c4120e'), 'Coffee Shops'),
+       (uuid_to_bin('812183c6-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849cc4-6d6b-11e9-8d1c-ca6496c4120e'), 'Fast Food'),
+       (uuid_to_bin('81218466-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849cc4-6d6b-11e9-8d1c-ca6496c4120e'), 'Groceries'),
+       (uuid_to_bin('812184fc-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849cc4-6d6b-11e9-8d1c-ca6496c4120e'), 'Restaurants');
+
+# Gifts & Donations
+insert into subcategory(id, category_id, name)
+values (uuid_to_bin('812915dc-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849ddc-6d6b-11e9-8d1c-ca6496c4120e'), 'Donations'),
+       (uuid_to_bin('8129194c-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849ddc-6d6b-11e9-8d1c-ca6496c4120e'), 'Gifts');
+
+# Health & Fitness
+insert into subcategory(id, category_id, name)
+values (uuid_to_bin('8130e6fe-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849ed6-6d6b-11e9-8d1c-ca6496c4120e'), 'Dentist'),
+       (uuid_to_bin('8130eac8-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849ed6-6d6b-11e9-8d1c-ca6496c4120e'), 'Doctor'),
+       (uuid_to_bin('8130ec9e-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849ed6-6d6b-11e9-8d1c-ca6496c4120e'), 'Eyecare'),
+       (uuid_to_bin('8130ed52-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849ed6-6d6b-11e9-8d1c-ca6496c4120e'), 'Gym'),
+       (uuid_to_bin('8130edfc-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849ed6-6d6b-11e9-8d1c-ca6496c4120e'), 'Health Insurance'),
+       (uuid_to_bin('8130ef1e-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849ed6-6d6b-11e9-8d1c-ca6496c4120e'), 'Medicine'),
+       (uuid_to_bin('8130efc8-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849ed6-6d6b-11e9-8d1c-ca6496c4120e'), 'Vitamins & Supplements');
+
+# Home
+insert into subcategory(id, category_id, name)
+values (uuid_to_bin('8139aff0-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849fe4-6d6b-11e9-8d1c-ca6496c4120e'), 'Home Improvement'),
+       (uuid_to_bin('8139b658-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849fe4-6d6b-11e9-8d1c-ca6496c4120e'), 'Home Insurance'),
+       (uuid_to_bin('8139b82e-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849fe4-6d6b-11e9-8d1c-ca6496c4120e'), 'Home Services'),
+       (uuid_to_bin('8139b950-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849fe4-6d6b-11e9-8d1c-ca6496c4120e'), 'Home Supplies'),
+       (uuid_to_bin('8139ba7c-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849fe4-6d6b-11e9-8d1c-ca6496c4120e'), 'Lawn & Garden'),
+       (uuid_to_bin('8139bb8a-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec849fe4-6d6b-11e9-8d1c-ca6496c4120e'), 'Mortgage & Rent');
+
+# Income
+insert into subcategory(id, category_id, name)
+values (uuid_to_bin('8141cbfe-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84a0f2-6d6b-11e9-8d1c-ca6496c4120e'), 'Bonus'),
+       (uuid_to_bin('8141d504-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84a0f2-6d6b-11e9-8d1c-ca6496c4120e'), 'Interest Income'),
+       (uuid_to_bin('8141d6ee-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84a0f2-6d6b-11e9-8d1c-ca6496c4120e'), 'Paycheck'),
+       (uuid_to_bin('8141d810-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84a0f2-6d6b-11e9-8d1c-ca6496c4120e'), 'Reimbursement'),
+       (uuid_to_bin('8141d8f6-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84a0f2-6d6b-11e9-8d1c-ca6496c4120e'), 'Rental Income'),
+       (uuid_to_bin('8141d9e6-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84a0f2-6d6b-11e9-8d1c-ca6496c4120e'), 'Returned Purchase'),
+       (uuid_to_bin('8141dacc-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84a0f2-6d6b-11e9-8d1c-ca6496c4120e'), 'Uncategorized Income');
+
+# Investments & Savings
+insert into subcategory(id, category_id, name)
+values (uuid_to_bin('8149b7b0-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84a1ec-6d6b-11e9-8d1c-ca6496c4120e'), 'Investments'),
+       (uuid_to_bin('8149bb20-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84a1ec-6d6b-11e9-8d1c-ca6496c4120e'), 'Savings');
+
+# Kids
+insert into subcategory(id, category_id, name)
+values (uuid_to_bin('8151a4c0-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84a368-6d6b-11e9-8d1c-ca6496c4120e'), 'Allowance'),
+       (uuid_to_bin('8151a858-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84a368-6d6b-11e9-8d1c-ca6496c4120e'), 'Baby Supplies'),
+       (uuid_to_bin('8151aa10-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84a368-6d6b-11e9-8d1c-ca6496c4120e'), 'Babysitter & Daycare'),
+       (uuid_to_bin('8151aad8-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84a368-6d6b-11e9-8d1c-ca6496c4120e'), 'Child Support'),
+       (uuid_to_bin('8151ab82-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84a368-6d6b-11e9-8d1c-ca6496c4120e'), 'Kids Activities'),
+       (uuid_to_bin('8151ac18-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84a368-6d6b-11e9-8d1c-ca6496c4120e'), 'Toys');
+
+# Personal Care
+insert into subcategory(id, category_id, name)
+values (uuid_to_bin('8157baf4-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84a5ac-6d6b-11e9-8d1c-ca6496c4120e'), 'Hair'),
+       (uuid_to_bin('8157cd78-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84a5ac-6d6b-11e9-8d1c-ca6496c4120e'), 'Face & Body');
+
+# Pets
+insert into subcategory(id, category_id, name)
+values (uuid_to_bin('815e76c8-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84a746-6d6b-11e9-8d1c-ca6496c4120e'), 'Pet Food & Supplies'),
+       (uuid_to_bin('815e7aba-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84a746-6d6b-11e9-8d1c-ca6496c4120e'), 'Pet Grooming'),
+       (uuid_to_bin('815e7bb4-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84a746-6d6b-11e9-8d1c-ca6496c4120e'), 'Veterinary');
+
+# Shopping
+insert into subcategory(id, category_id, name)
+values (uuid_to_bin('8166097e-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84a8ea-6d6b-11e9-8d1c-ca6496c4120e'), 'Books'),
+       (uuid_to_bin('81660cbc-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84a8ea-6d6b-11e9-8d1c-ca6496c4120e'), 'Clothing'),
+       (uuid_to_bin('81660d98-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84a8ea-6d6b-11e9-8d1c-ca6496c4120e'), 'Electronics & Software'),
+       (uuid_to_bin('81660e38-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84a8ea-6d6b-11e9-8d1c-ca6496c4120e'), 'Hobbies'),
+       (uuid_to_bin('81660ed8-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84a8ea-6d6b-11e9-8d1c-ca6496c4120e'), 'Misc Shopping');
+
+# Taxes
+insert into subcategory(id, category_id, name)
+values (uuid_to_bin('816e15e2-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84aaa2-6d6b-11e9-8d1c-ca6496c4120e'), 'Federal Tax'),
+       (uuid_to_bin('816e1934-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84aaa2-6d6b-11e9-8d1c-ca6496c4120e'), 'Local Tax'),
+       (uuid_to_bin('816e1ace-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84aaa2-6d6b-11e9-8d1c-ca6496c4120e'), 'Property Tax'),
+       (uuid_to_bin('816e1b8c-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84aaa2-6d6b-11e9-8d1c-ca6496c4120e'), 'Sales Tax'),
+       (uuid_to_bin('816e1c22-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84aaa2-6d6b-11e9-8d1c-ca6496c4120e'), 'State Tax');
+
+# Transfer
+insert into subcategory(id, category_id, name)
+values (uuid_to_bin('817614ea-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84ac3c-6d6b-11e9-8d1c-ca6496c4120e'), 'Credit Card Payment'),
+       (uuid_to_bin('8176185a-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84ac3c-6d6b-11e9-8d1c-ca6496c4120e'), 'Transfer for Cash Spending');
+
+# Travel
+insert into subcategory(id, category_id, name)
+values (uuid_to_bin('817cd78a-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84ad9a-6d6b-11e9-8d1c-ca6496c4120e'), 'Air Travel'),
+       (uuid_to_bin('817cdad2-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84ad9a-6d6b-11e9-8d1c-ca6496c4120e'), 'Hotel'),
+       (uuid_to_bin('817cdc80-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84ad9a-6d6b-11e9-8d1c-ca6496c4120e'), 'Rental Car & Taxi'),
+       (uuid_to_bin('817cdd2a-6d6f-11e9-8d1c-ca6496c4120e'), uuid_to_bin('ec84ad9a-6d6b-11e9-8d1c-ca6496c4120e'), 'Vacation');
