ce890b0248b8fa0d74267244a20d9276d2f708db
Isaac <isaacgarza432@gmail.com>
2019-05-06 22:51:32 -0500

    GH-13: Created initial backend application using Spring Boot

---
+++ backend/src/test/java/com/spendsmart/SpendSmartBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spendsmart.Spend.Smart.Backend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpendSmartBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
