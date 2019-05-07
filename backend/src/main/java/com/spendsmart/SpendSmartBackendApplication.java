00a5753d9c7d3cec83324937758899517ab4cedc
Isaac <isaacgarza432@gmail.com>
2019-05-06 22:51:32 -0500

    GH-13: Created initial backend application using Spring Boot

---
+++ backend/src/main/java/com/spendsmart/SpendSmartBackendApplication.java
@@ -0,0 +1,12 @@
+package com.spendsmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendSmartBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendSmartBackendApplication.class, args);
+	}
+}
