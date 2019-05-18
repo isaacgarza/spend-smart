7352c307dfb71d9e6fd7dca076dc4ebce0a89115
weida8 <wei-dapan@wei-das-air.lan>
2019-05-17 19:09:21 -0500

    GH-24: eslint with prettier  - SignIn.js was reformatted by prettier  - .estlintrc added for eslint config

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -1,56 +1,64 @@
-import React, { Component } from 'react';
-import {Form, Button, Card} from "react-bootstrap"
+import React, { Component } from "react";
+import { Form, Button } from "react-bootstrap";
 
 class SignIn extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
 
-    constructor() {
-        super();
-        this.state = {
-            email: "",
-            password: ""
-        }
-    }
+  handleEmail = e => {
+    this.setState({
+      email: e.target.value
+    });
+  };
 
-    handleEmail = e => {
-        this.setState({
-            email: e.target.value
-        })
-    }
+  handlePassword = e => {
+    this.setState({
+      password: e.target.value
+    });
+  };
 
-    handlePassword = e => {
-        this.setState({
-            password: e.target.value
-        })
-    }
+  handleOnSubmit = e => {
+    alert("Signed Up!");
+    e.preventDefault();
+    //API to save user to data base in the future
+  };
 
-    handleOnSubmit = e => {
-        alert("Signed Up!");
-        e.preventDefault();
-        //API to save user to data base in the future
-    }
+  render() {
+    return (
+      <Form onSubmit={this.handleOnSubmit}>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={this.state.email}
+            onChange={this.handleEmail}
+          />
+          <Form.Text className="text-muted">
+            We'll sell your email to the highest bidder.
+          </Form.Text>
+        </Form.Group>
 
-    render() {
-        return (
-            <Form onSubmit={this.handleOnSubmit}>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" value={this.state.email} onChange={this.handleEmail} />
-                    <Form.Text className="text-muted">
-                    We'll sell your email to the highest bidder.
-                    </Form.Text>
-                </Form.Group>
-
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" value={this.state.password} onChange={this.handlePassword}/>
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Sign In
-                </Button>
-            </Form>
-        );
-    }
-
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.handlePassword}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Sign In
+        </Button>
+      </Form>
+    );
+  }
 }
 
 export default SignIn;
