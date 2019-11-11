1cf604ae9f00f8599011eec727ca33c05941d3b4
weida8 <at4214@yahoo.com.tw>
2019-11-11 17:01:19 -0600

    GH-30: restyling nav links

--- client/src/components/NavHeader.jsx
+++ client/src/components/NavHeader.jsx
@@ -1,29 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar } from "react-bootstrap";
+import styled from 'styled-components';
 
+const NavContainer = styled.div`
+  .title {
+    color: white;
+  }
+  .links {
+    padding-left: 20px;
+  }
+`;
 export default function NavHeader() {
   return (
-    <Navbar
-      collapseOnSelect="collapseOnSelect"
-      expand="lg"
-      bg="dark"
-      variant="dark"
-    >
-      <Navbar.Brand>
-        <Link to="/">SPEND SMART</Link>
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link>
-            <Link to="/sign-in">Sign In</Link>
-          </Nav.Link>
-          <Nav.Link>
-            <Link to="/sign-up">Sign Up</Link>
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <NavContainer>
+      <Navbar
+        collapseOnSelect="collapseOnSelect"
+        expand="lg"
+        bg="dark"
+        variant="dark"
+      >
+        <Navbar.Brand >
+          <Link className="title" to="/">SPEND SMART</Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse className="justify-content-end" id="responsive-navbar-nav">
+            <Navbar.Text className="links">
+              <Link to="/sign-in">Sign In</Link>
+            </Navbar.Text>
+            <Navbar.Text className="links">
+              <Link to="/sign-up">Sign Up</Link>
+            </Navbar.Text>
+        </Navbar.Collapse>
+      </Navbar>
+    </NavContainer>
   );
 }
